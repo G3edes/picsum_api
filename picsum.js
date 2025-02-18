bc0172c2aaa834981fcab4aaf7163d3d7a66c03a
GABRIEL SILVA GUEDES <24122379@CFP127ALUNO.LOCAL>
2025-02-18 11:59:31 -0300

    api_picsum

---
+++ picsum.js
@@ -0,0 +1,26 @@
+'use strict'
+
+async function pesquisarFotos() {
+    const url=`https://picsum.photos/v2/list?page=2&limit=50`
+    const response = await fetch(url)
+    const date= await response.json()
+
+    return date
+}
+
+function criarImagem(link){
+    const galeria=document.getElementById('galeria')
+    const novaImg=document.createElement('img')
+    novaImg.src=link.download_url    
+    galeria.appendChild(novaImg)
+}
+
+async function preencherFotos() {
+    const fotos= await pesquisarFotos()
+    const galeria=document.getElementById('galeria')
+    galeria.replaceChildren('')
+    fotos.forEach(criarImagem)
+    console.log(fotos)
+}
+
+preencherFotos()
